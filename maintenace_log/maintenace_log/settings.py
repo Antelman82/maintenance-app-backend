16b4f99a64cf13ec22e327939eb0e1b2bfd8e6b0
Tony Antelman <AntelmanAnthonyJ@JohnDeere.com>
2020-04-14 17:28:31 -0500

    updated models to display better in admin view

--- maintenace_log/maintenace_log/settings.py
+++ maintenace_log/maintenace_log/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'logs',
-    'rest_framework'
+    'rest_framework',
+    'django_extensions',
+    'phone_field'
 ]
 
 MIDDLEWARE = [
